1ebe704c184e58bdf75666fd530a0fad883cf48f
Allan Jeremy <aj.dev254@gmail.com>
2017-01-27 10:39:59 +0300

    Restructured the project Documents are now in the root folder of the repository

---
+++ index.php
@@ -0,0 +1,125 @@
+ <!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+      <link type="text/css" rel="stylesheet" href="css/blackgold.css"/>
+     
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+      <meta name="description" content="BlackGold Ventures Worldwide is a venture company focused on leveraging technology and Innovative entrepreneurship
+Using today's pioneering technologies to solve problems and create value in today's
+ highly competitive inter-connected global market."/>
+      <meta keywords="venture,capital,company,africa,worldwide,drones,corporate,black,gold">
+      <title>BlackGold Ventures Worldwide : Explore | Dream | Inspire</title>
+    </head>
+
+    <body class="grey darken-4">
+      <main>
+<!--          Landing image -->
+
+        <div class="parallax-container valign-wrapper" id="landing_img">
+            <div class="container center">
+              <h2 class="valign center-aligned grey-text text-lighten-2 valign hide-on-small-only">BLACK<span class="amber-text">GOLD</span> VENTURES WORLDWIDE</h2>
+              <h5 class="valign center-aligned grey-text text-lighten-2 valign hide-on-med-and-up">BLACK<span class="amber-text">GOLD</span> VENTURES WORLDWIDE</h5>
+            </div>
+          <div class="parallax" >
+              <img class="img-responsive" src="media/landing.jpg" alt="Blackgold Ventures Worldwide | Landing Image" width="100%"/>
+          </div>
+        </div>
+
+<!--          About -->
+          <div class="row">
+            <div class="col s12 m8 push-m2">
+                <h4 class="center grey-text text-lighten-2 hide-on-small-only">Explore | <span class="amber-text">Dream</span> | Discover</h4> 
+                <h5 class="center grey-text text-lighten-2 hide-on-med-and-up">Explore | <span class="amber-text">Dream</span> | Discover</h5> 
+                
+                <h5 class="grey-text hide-on-small-only">BlackGold Ventures Worldwide is a venture company focused on leveraging technology and Innovative entrepreneurship
+Using today's pioneering technologies to solve problems and create value in today's
+ highly competitive inter-connected global market.</h5>
+                
+                <h6 class="grey-text hide-on-med-and-up">BlackGold Ventures Worldwide is a venture company focused on leveraging technology and Innovative entrepreneurship
+Using today's pioneering technologies to solve problems and create value in today's
+ highly competitive inter-connected global market.</h6>
+            </div>
+          </div>
+        <div class="parallax-container" id="corporate_img">
+         <div class="parallax">
+             <img src="media/corporate.jpg" alt="Blackgold Ventures Worldwide | Corporate" width="100%"/>
+        </div>
+
+        </div>
+          <div class="container">
+          <div class="row">
+              <div class="col s12 m10 push-m1 l6" id="contact_info">
+                    <h3 class="grey-text text-lighten-2 hide-on-med-and-down">Contact Information</h3>
+                    <h4 class="grey-text text-lighten-2 hide-on-large-only">Contact Info<span class="hide-on-small-only">rmation</span></h4>
+                  
+                    <h5 class="grey-text text-lighten-2 hide-on-small-only">Email  <br><small class="grey-text">info@blackgoldventures.com</small></h5>
+                    <br class="hidden-on-small-only"><div class="divider grey darken-3 hide-on-small-only"></div>
+                  
+                    <h5 class="grey-text text-lighten-2 hide-on-small-only">Phone <br> <small class="grey-text">+123 456 772 112</small></h5>  
+                    <br class="hidden-on-small-only"><div class="divider grey darken-3 hide-on-small-only"></div>
+                  
+                    <h5 class="grey-text text-lighten-2 hide-on-small-only">Office Locations  <br><small class="grey-text">Some location,Kenya</small></h5>
+                    <br class="hidden-on-small-only"><div class="divider grey darken-3 hide-on-small-only"></div>
+                    
+                    
+                    <h6 class="grey-text text-lighten-2 hide-on-med-and-up">Email : <span class="grey-text">info@blackgoldventures.com</span></h6>
+                    <h6 class="grey-text text-lighten-2 hide-on-med-and-up">Phone : <span class="grey-text">+123 456 772 112</span></h6>
+                    <h6 class="grey-text text-lighten-2 hide-on-med-and-up">Office Location : <span class="grey-text">Some location,Kenya</span></h6>
+              </div>
+              <div class="col s12 m10 push-m1 l6 row" id="send_message">
+                  <br class="hide-on-med-and-up"><div class="divider hide-on-med-and-up grey darken-3"></div><br class="hide-on-med-and-up">
+                  <div class="col s12">
+                      <h3 class="grey-text text-lighten-2 hide-on-med-and-down">Send us a message</h3>
+                      <h4 class="grey-text text-lighten-2 hide-on-large-only">Message us</h4>
+                  </div>
+                  <form class="col s12 row">
+                      <div class="input-field col s12 m6">
+                          <label for="name_input">Your Name(s)</label>
+                         <input type="text" id="name_input" name="contact_name" class="validate grey-text text-lighten-1" required>
+                      </div>
+                      <div class="input-field col s12 m6">
+                          <label for="email_input">Email Address</label>
+                         <input type="email" id="name_input" name="email_input" class="validate grey-text text-lighten-1" required>
+                      </div>
+                      <div class="input-field col s12">
+                          <label for="message_input">Your Message</label>
+                          <textarea  id="message_input" name="contact_message" class="materialize-textarea validate grey-text text-lighten-1" required></textarea>
+                      </div>
+                      <div class="col s6">
+                         <input type="radio" id="rep_individual_input" name="contact_rep" class="validate" checked>
+                         <label for="rep_individual_input">Individual <span class="hide-on-small-only">Rep.</span></label>
+                      </div>
+                      <div class="col s6 ">
+                         <input type="radio" id="rep_company_input" name="contact_rep" class="validate amber-text">
+                         <label for="rep_company_input" >Company <span class="hide-on-small-only">Rep.</span></label>
+                      </div>
+                      
+                      <div class="container col s12 center">
+                          <br>
+                          <button class="btn" type="submit">Send</button>
+                      </div>
+                  </form>
+              </div>
+          </div> 
+          </div>
+
+          <div class="divider grey darken-3"></div>
+      </main>
+        
+     <footer>
+        <p class="center grey-text">&copy; <?php echo date("Y")?> BlackGold Ventures Worldwide LLC. </p>
+        <p class="center grey-text text-darken-2">Developed by <small><a href="http://www.deflix.org">Deflix Enterprises</a></small></p>
+     </footer>
+      <script type="text/javascript" src="js/jquery-2.1.1.min.js"></script>
+        <script type="text/javascript" src="js/materialize.min.js"></script>
+<script>
+    $(document).ready(function(){
+      $('.parallax').parallax();
+    });
+</script>
+    </body>
+  </html>
+        
